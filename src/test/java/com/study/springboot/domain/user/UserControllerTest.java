bfc4f4e5a3ee5b057519e4dec88ecc2abd86c4a8
jennyjoo <100cllr@gmail.com>
2024-05-09 13:06:06 +0900

    feat : userController test 생성

---
+++ src/test/java/com/study/springboot/domain/user/UserControllerTest.java
@@ -0,0 +1,11 @@
+package com.study.springboot.domain.user;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+}
